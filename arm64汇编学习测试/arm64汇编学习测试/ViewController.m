6abd847f7af43b931444f38b76f42f5d9b81fec4
zhongchenghuan <chenghuan.zhong@aqara.com>
2021-05-27 20:17:44 +0800

    创建arm64

--- arm64汇编学习测试/arm64汇编学习测试/ViewController.m
+++ arm64汇编学习测试/arm64汇编学习测试/ViewController.m
@@ -7,16 +7,38 @@
 
 #import "ViewController.h"
 
+void cTest(int a){
+    
+    int b = 20;
+    int c = 30;
+    printf("%d",a);
+    
+}
+
 @interface ViewController ()
 
 @end
+
+
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self testDissembly];
     // Do any additional setup after loading the view.
 }
+
+-(void)testDissembly{
+    __asm volatile ("mov x0, #16\n");
+    __asm volatile ("mov x0, #16\n");
+    cTest(20);
+    NSLog(@"");
+}
+
+
+
+
 
 
 @end
